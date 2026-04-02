aa1bcb261af1ad9fd61e87b4c42310bcec11ea2d
abhirag <abhirag4169@gmail.com>
2026-04-02 15:46:06 +0530

    theme error fixed

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -5,25 +5,27 @@ import { Switch } from "@/components/ui/switch"
 import { useEffect, useState } from "react"
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { resolvedTheme, setTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
-
 
   useEffect(() => {
     setMounted(true)
   }, [])
 
-
   if (!mounted) return null
+
+  const isDark = resolvedTheme === "dark"
 
   return (
     <div className="flex items-center gap-2">
       <span className="text-sm">Dark</span>
+
       <Switch
-        checked={theme === "dark"}
-        onCheckedChange={(val) => setTheme(val ? "dark" : "light")}
+        checked={isDark}
+        onCheckedChange={(val) =>
+          setTheme(val ? "dark" : "light")
+        }
       />
     </div>
   )
 }
-
